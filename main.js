63166d240b8792d05f1b0d241d7b3f8b870cbdc6
Daniel Potts <djpotts2891@gmail.com>
2023-06-04 21:07:01 +0000

    tabular data complete with truncated data

--- main.js
+++ main.js
@@ -23,11 +23,10 @@ function getTableHeaders(obj) {
 };
 
 function writeToDocument(type) {
+    var tableRows = [];
     var el = document.getElementById("data");
-    el.innerHTML = "";
 
     getData(type, function(data) {
-        var tableRows = [];
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
@@ -35,12 +34,16 @@ function writeToDocument(type) {
            var dataRow = [];
 
            Object.keys(item).forEach(function(key) {
-            dataRow.push(`<td>${item[key]}</td>`);
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15)
+                dataRow.push(`<td>${truncatedData}</td>`);
            });
-           tableRows.push(dataRow)
+
+           tableRows.push(`<tr>${dataRow}</tr>`)
+
         });
 
         el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
         
     });
-};
+}
